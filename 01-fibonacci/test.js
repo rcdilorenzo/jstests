f623229bd9d6f4cae56979ba65a7850b4c3d2d74
Christian Di Lorenzo <rcdilorenzo@me.com>
2015-07-20 18:36:35 -0400

    Initial test

---
+++ 01-fibonacci/test.js
@@ -0,0 +1,22 @@
+var expect = require('expect.js')
+
+// <--- INSERT CODE HERE --->
+
+var enableBonusTest = false;
+
+
+// ---
+
+describe('fibonacci sequence', function() {
+    it('should return nth term', function () {
+        expect(fib(1)).to.be(1);
+        expect(fib(5)).to.be(5);
+        expect(fib(6)).to.be(8);
+    });
+
+    var bonusTest = enableBonusTest ? it : it.skip;
+    bonusTest('BONUS: should return the set of first n terms', function () {
+        expect(fib_set(2)).to.eql([1, 1]);
+        expect(fib_set(5)).to.eql([1, 1, 2, 3, 5]);
+    });
+});
